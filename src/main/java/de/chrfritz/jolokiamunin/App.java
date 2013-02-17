54abe916b061c2e7684c2e8f0e7e4345e1284aa0
Christian Fritz <mail@chr-fritz.de>
2013-02-17 21:15:16 +0100

    Main Project Structure

---
+++ src/main/java/de/chrfritz/jolokiamunin/App.java
@@ -0,0 +1,13 @@
+package de.chrfritz.jolokiamunin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
