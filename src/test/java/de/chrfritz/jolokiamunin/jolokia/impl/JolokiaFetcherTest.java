ee8d07fca5144dba9bfb8e1a8194a697c269d29b
Christian Fritz <mail@chr-fritz.de>
2013-02-20 21:51:13 +0100

    - Extend the jolokia test with test data - Added Mockito and Json-simple to test dependencies

--- src/test/java/de/chrfritz/jolokiamunin/jolokia/impl/JolokiaFetcherTest.java
+++ src/test/java/de/chrfritz/jolokiamunin/jolokia/impl/JolokiaFetcherTest.java
@@ -2,24 +2,31 @@ package de.chrfritz.jolokiamunin.jolokia.impl;
 
 import de.chrfritz.jolokiamunin.jolokia.Fetcher;
 import de.chrfritz.jolokiamunin.jolokia.Request;
+import org.jolokia.client.J4pClient;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
-import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(MockitoJUnitRunner.class)
 public class JolokiaFetcherTest {
     private Fetcher fetcher;
+    @Mock
+    private J4pClient client;
 
     @Before
     public void setUp() throws Exception {
-        fetcher = new JolokiaFetcher();
-        fetcher.setUrl(new URL("http://localhost:8080/jolokia/"));
+
+        fetcher = new JolokiaFetcher(client);
     }
 
     @After
@@ -28,14 +35,51 @@ public class JolokiaFetcherTest {
     }
 
     @Test
-    public void testFetchValues() throws Exception {
+    public void testFetchValuesFull() throws Exception {
 
-        List<Request> requestList = new ArrayList<Request>();
-        requestList.add(new Request("java.lang:type=Memory", "HeapMemoryUsage"));
+        Map<Request, Number> expected = new HashMap<>();
+        expected.put(new Request("java.lang:type=Memory", "NonHeapMemoryUsage", "used"), 1000);
+        expected.put(new Request("java.lang:type=Memory", "NonHeapMemoryUsage", "committed"), 1000);
+
+        List<Request> requestList = new ArrayList<>();
         requestList.add(new Request("java.lang:type=Memory", "NonHeapMemoryUsage", "used"));
         requestList.add(new Request("java.lang:type=Memory", "NonHeapMemoryUsage", "committed"));
         Map<Request, Number> responses = fetcher.fetchValues(requestList);
 
-        assertEquals(6, responses.size());
+        assertEquals(expected, responses);
+    }
+
+    @Test
+    public void testFetchValuesWithoutPath() throws Exception {
+        Map<Request, Number> expected = new HashMap<>();
+        expected.put(new Request("java.lang:type=Memory", "HeapMemoryUsage", "max"), 1000);
+        expected.put(new Request("java.lang:type=Memory", "HeapMemoryUsage", "committed"), 1000);
+        expected.put(new Request("java.lang:type=Memory", "HeapMemoryUsage", "init"), 1000);
+        expected.put(new Request("java.lang:type=Memory", "HeapMemoryUsage", "used"), 1000);
+
+        List<Request> requestList = new ArrayList<>();
+        requestList.add(new Request("java.lang:type=Memory", "HeapMemoryUsage"));
+        Map<Request, Number> responses = fetcher.fetchValues(requestList);
+
+        assertEquals(expected, responses);
+    }
+
+    @Test
+    public void testFetchValueWithoutAttribute() throws Exception {
+        Map<Request, Number> expected = new HashMap<>();
+        expected.put(new Request("java.lang:type=Memory", "HeapMemoryUsage", "max"), 1000);
+        expected.put(new Request("java.lang:type=Memory", "HeapMemoryUsage", "committed"), 1000);
+        expected.put(new Request("java.lang:type=Memory", "HeapMemoryUsage", "init"), 1000);
+        expected.put(new Request("java.lang:type=Memory", "HeapMemoryUsage", "used"), 1000);
+        expected.put(new Request("java.lang:type=Memory", "NonHeapMemoryUsage", "max"), 1000);
+        expected.put(new Request("java.lang:type=Memory", "NonHeapMemoryUsage", "committed"), 1000);
+        expected.put(new Request("java.lang:type=Memory", "NonHeapMemoryUsage", "init"), 1000);
+        expected.put(new Request("java.lang:type=Memory", "NonHeapMemoryUsage", "used"), 1000);
+
+        List<Request> requestList = new ArrayList<>();
+        requestList.add(new Request("java.lang:type=Memory"));
+        Map<Request, Number> responses = fetcher.fetchValues(requestList);
+
+        assertEquals(expected, responses);
     }
 }
