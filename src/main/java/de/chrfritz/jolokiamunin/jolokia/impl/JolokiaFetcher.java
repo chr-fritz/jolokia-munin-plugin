74cea5692ebcdcaa3938e7d12152a98055f4cdca
Christian Fritz <mail@chr-fritz.de>
2013-02-23 19:38:40 +0100

    Fully implemented jolokia fetcher

--- src/main/java/de/chrfritz/jolokiamunin/jolokia/impl/JolokiaFetcher.java
+++ src/main/java/de/chrfritz/jolokiamunin/jolokia/impl/JolokiaFetcher.java
@@ -4,18 +4,18 @@ import com.google.common.base.Strings;
 import de.chrfritz.jolokiamunin.jolokia.Fetcher;
 import de.chrfritz.jolokiamunin.jolokia.FetcherException;
 import de.chrfritz.jolokiamunin.jolokia.Request;
+import org.apache.commons.lang3.StringUtils;
 import org.jolokia.client.J4pClient;
 import org.jolokia.client.exception.J4pException;
 import org.jolokia.client.request.J4pReadRequest;
 import org.jolokia.client.request.J4pReadResponse;
+import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.management.MalformedObjectNameException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class JolokiaFetcher implements Fetcher {
 
@@ -24,7 +24,7 @@ public class JolokiaFetcher implements Fetcher {
     private J4pClient client;
 
 
-    public JolokiaFetcher(URL url){
+    public JolokiaFetcher(URL url) {
         client = new J4pClient(url.toString());
     }
 
@@ -36,17 +36,68 @@ public class JolokiaFetcher implements Fetcher {
     public Map<Request, Number> fetchValues(List<Request> requests) throws FetcherException {
 
         List<J4pReadRequest> readRequests = createReadRequests(requests);
+        Map<Request, Number> results = new HashMap<>();
         try {
             List<J4pReadResponse> responses = client.execute(readRequests);
 
-            responses.size();
+            int i = 0;
+            for (J4pReadResponse response : responses) {
+                Object value = response.getValue();
+                LOGGER.debug("Response value ({}): {}", value.getClass(), value);
+                LOGGER.debug("Attributes {}", response.getAttributes());
+                Request request = requests.get(i);
+
+                boolean isValid = StringUtils.equals(request.getMbean(),
+                                                     response.getRequest().getObjectName().toString()) &&
+                        StringUtils.equals(request.getAttribute(), response.getRequest().getAttribute()) &&
+                        StringUtils.equals(request.getPath(), response.getRequest().getPath());
+
+                if (!isValid) {
+                    continue;
+                }
+
+                if (!Strings.isNullOrEmpty(request.getMbean()) && !Strings.isNullOrEmpty(
+                        request.getAttribute()) && !Strings.isNullOrEmpty(request.getPath())) {
+                    results.put(request, (Number) value);
+                } else if (!Strings.isNullOrEmpty(request.getMbean()) && !Strings.isNullOrEmpty(
+                        request.getAttribute()) && Strings.isNullOrEmpty(request.getPath())) {
+                    handleAttributeResponse(results, (JSONObject) value, request);
+                } else if (!Strings.isNullOrEmpty(request.getMbean()) && Strings.isNullOrEmpty(
+                        request.getAttribute())) {
+                    for (Map.Entry entry : (Set<Map.Entry>) ((JSONObject) value).entrySet()) {
+                        LOGGER.debug("{}", entry);
+                        if (entry.getValue() instanceof JSONObject) {
+                            handleAttributeResponse(results, (JSONObject) entry.getValue(),
+                                                    new Request(request.getMbean(), (String) entry.getKey()));
+                        } else if (entry.getValue() instanceof Number) {
+                            results.put(new Request(request.getMbean(), (String) entry.getKey()),
+                                        (Number) entry.getValue());
+                        }
+                    }
+                }
+
+                i++;
+            }
         }
         catch (J4pException e) {
             LOGGER.error("", e);
             throw new FetcherException(e);
         }
 
-        return null;
+        return results;
+    }
+
+    private void handleAttributeResponse(Map<Request, Number> results, JSONObject values, Request request) {
+
+        for (Map.Entry entry : (Set<Map.Entry>) values.entrySet()) {
+
+            if (!(entry.getValue() instanceof Number)) {
+                continue;
+            }
+            LOGGER.debug("{}:{}", entry.getKey(), entry.getValue());
+            results.put(new Request(request.getMbean(), request.getAttribute(), (String) entry.getKey()),
+                        (Number) entry.getValue());
+        }
     }
 
     private List<J4pReadRequest> createReadRequests(List<Request> requests) {
