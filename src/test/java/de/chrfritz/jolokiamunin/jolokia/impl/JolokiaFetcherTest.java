dab0d5eb0b91047a107bfb1bb8245b20f5eb564d
Christian Fritz <mail@chr-fritz.de>
2013-02-23 18:07:19 +0100

    Extended Jolokia Fetcher Test

--- src/test/java/de/chrfritz/jolokiamunin/jolokia/impl/JolokiaFetcherTest.java
+++ src/test/java/de/chrfritz/jolokiamunin/jolokia/impl/JolokiaFetcherTest.java
@@ -2,6 +2,13 @@ package de.chrfritz.jolokiamunin.jolokia.impl;
 
 import de.chrfritz.jolokiamunin.jolokia.Fetcher;
 import de.chrfritz.jolokiamunin.jolokia.Request;
+import org.apache.http.HttpResponse;
+import org.apache.http.ProtocolVersion;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.InputStreamEntity;
+import org.apache.http.message.BasicHttpResponse;
+import org.apache.http.message.BasicStatusLine;
 import org.jolokia.client.J4pClient;
 import org.junit.After;
 import org.junit.Before;
@@ -10,23 +17,27 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class JolokiaFetcherTest {
     private Fetcher fetcher;
     @Mock
-    private J4pClient client;
+    private HttpClient client;
 
     @Before
     public void setUp() throws Exception {
 
-        fetcher = new JolokiaFetcher(client);
+        fetcher = new JolokiaFetcher(new J4pClient("", client));
     }
 
     @After
@@ -34,9 +45,24 @@ public class JolokiaFetcherTest {
         fetcher = null;
     }
 
+    private void injectResponse(int expectedResponseStatus, String responseFile) throws
+            IOException {
+
+        InputStream stream = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream(responseFile);
+
+        HttpResponse response = new BasicHttpResponse(
+                new BasicStatusLine(new ProtocolVersion("HTTP", 1, 1), expectedResponseStatus, ""));
+        response.setStatusCode(expectedResponseStatus);
+        response.setEntity(new InputStreamEntity(stream, Long.MAX_VALUE));
+
+        when(client.execute((HttpUriRequest) any())).thenReturn(response);
+    }
+
     @Test
     public void testFetchValuesFull() throws Exception {
 
+        injectResponse(200, "de/chrfritz/jolokiamunin/jolokia/impl/fetchValuesFull.json");
         Map<Request, Number> expected = new HashMap<>();
         expected.put(new Request("java.lang:type=Memory", "NonHeapMemoryUsage", "used"), 1000);
         expected.put(new Request("java.lang:type=Memory", "NonHeapMemoryUsage", "committed"), 1000);
@@ -51,6 +77,7 @@ public class JolokiaFetcherTest {
 
     @Test
     public void testFetchValuesWithoutPath() throws Exception {
+        injectResponse(200, "de/chrfritz/jolokiamunin/jolokia/impl/fetchValuesWithoutPath.json");
         Map<Request, Number> expected = new HashMap<>();
         expected.put(new Request("java.lang:type=Memory", "HeapMemoryUsage", "max"), 1000);
         expected.put(new Request("java.lang:type=Memory", "HeapMemoryUsage", "committed"), 1000);
@@ -65,7 +92,8 @@ public class JolokiaFetcherTest {
     }
 
     @Test
-    public void testFetchValueWithoutAttribute() throws Exception {
+    public void testFetchValuesWithoutAttribute() throws Exception {
+        injectResponse(200, "de/chrfritz/jolokiamunin/jolokia/impl/fetchValuesWithoutAttirbute.json");
         Map<Request, Number> expected = new HashMap<>();
         expected.put(new Request("java.lang:type=Memory", "HeapMemoryUsage", "max"), 1000);
         expected.put(new Request("java.lang:type=Memory", "HeapMemoryUsage", "committed"), 1000);
@@ -78,6 +106,26 @@ public class JolokiaFetcherTest {
 
         List<Request> requestList = new ArrayList<>();
         requestList.add(new Request("java.lang:type=Memory"));
+        Map<Request, Number> responses = fetcher.fetchValues(requestList);
+
+        assertEquals(expected, responses);
+    }
+
+    @Test
+    public void testFetchValuesMixed() throws Exception {
+        injectResponse(200, "de/chrfritz/jolokiamunin/jolokia/impl/fetchValuesMixed.json");
+        Map<Request, Number> expected = new HashMap<>();
+        expected.put(new Request("java.lang:type=Memory", "HeapMemoryUsage", "max"), 1000);
+        expected.put(new Request("java.lang:type=Memory", "HeapMemoryUsage", "committed"), 1000);
+        expected.put(new Request("java.lang:type=Memory", "HeapMemoryUsage", "init"), 1000);
+        expected.put(new Request("java.lang:type=Memory", "HeapMemoryUsage", "used"), 1000);
+        expected.put(new Request("java.lang:type=Memory", "NonHeapMemoryUsage", "committed"), 1000);
+        expected.put(new Request("java.lang:type=Memory", "NonHeapMemoryUsage", "used"), 1000);
+
+        List<Request> requestList = new ArrayList<>();
+        requestList.add(new Request("java.lang:type=Memory", "HeapMemoryUsage"));
+        requestList.add(new Request("java.lang:type=Memory", "NonHeapMemoryUsage", "used"));
+        requestList.add(new Request("java.lang:type=Memory", "NonHeapMemoryUsage", "committed"));
         Map<Request, Number> responses = fetcher.fetchValues(requestList);
 
         assertEquals(expected, responses);
