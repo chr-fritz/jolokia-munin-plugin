c453b0d2f8c178b65a4528007bccdf1ecf20e928
Christian Fritz <mail@chr-fritz.de>
2013-12-31 16:48:33 +0100

    Config Controller with test

---
+++ src/main/java/de/chrfritz/jolokiamunin/controller/ConfigController.java
@@ -0,0 +1,16 @@
+package de.chrfritz.jolokiamunin.controller;
+
+import de.chrfritz.jolokiamunin.config.Configuration;
+
+public class ConfigController extends AbstractController {
+    @Override
+    protected String handle() throws Exception {
+        Configuration config = getConfiguration();
+        return getMuninProvider().getConfig(config.getConfiguration());
+    }
+
+    @Override
+    public String getHelpMessage() {
+        return "Get the configuration for munin";
+    }
+}
---
+++ src/test/java/de/chrfritz/jolokiamunin/controller/ConfigControllerTest.java
@@ -0,0 +1,54 @@
+// ______________________________________________________________________________
+//
+//           Project: jolokia-munin-plugin
+//            Module: jolokia-munin-plugin
+//             Class: ConfigControllerTest
+//              File: ConfigControllerTest.java
+//        changed by: christian
+//       change date: 31.12.13 16:21
+//       description:
+// ______________________________________________________________________________
+//
+//         Copyright: (c) Christian Fritz, all rights reserved
+// ______________________________________________________________________________
+
+package de.chrfritz.jolokiamunin.controller;
+
+import de.chrfritz.jolokiamunin.config.Category;
+import de.chrfritz.jolokiamunin.config.Configuration;
+import de.chrfritz.jolokiamunin.munin.MuninProvider;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertSame;
+import static org.mockito.Matchers.anyListOf;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ConfigControllerTest {
+
+    @Mock
+    private MuninProvider provider;
+
+    @Mock
+    private Configuration configuration;
+
+    private Controller controller = new ConfigController();
+
+    @Before
+    public void setUp() throws Exception {
+        controller.setMuninProvider(provider);
+        controller.setConfiguration(configuration);
+    }
+
+    @Test
+    public void testExecute() throws Exception {
+        String expected = "Sample Config";
+        when(provider.getConfig(anyListOf(Category.class))).thenReturn(expected);
+        String actual = controller.execute("");
+        assertSame(expected, actual);
+    }
+}
