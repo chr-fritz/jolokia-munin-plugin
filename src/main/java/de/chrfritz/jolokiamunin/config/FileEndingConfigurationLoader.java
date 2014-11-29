3173c381f8af2c41a08a8670693f1df6877f68e9
Christian Fritz <mail@chr-fritz.de>
2014-11-29 17:47:36 +0100

    New Configuration loader "FileEndingConfigLoader"

---
+++ src/main/java/de/chrfritz/jolokiamunin/config/FileEndingConfigurationLoader.java
@@ -0,0 +1,85 @@
+// ______________________________________________________________________________
+//
+//           Project: jolokia-munin-plugin
+//            Module: jolokia-munin-plugin
+//             Class: FileEndingConfigurationLoader
+//              File: FileEndingConfigurationLoader.java
+//        changed by: christian.fritz
+//       change date: 29.11.14 16:47
+// ______________________________________________________________________________
+//
+//         Copyright: (c) Christian Fritz, all rights reserved
+// ______________________________________________________________________________
+
+package de.chrfritz.jolokiamunin.config;
+
+import com.google.common.collect.Lists;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.ServiceLoader;
+
+/**
+ * Load a configuration using a configuration loader based by the file ending of the given config file.
+ * <p>
+ * It uses the {@link java.util.ServiceLoader} to find the available configuration loader. If you want to implement your
+ * own configuration loader just implement the {@link de.chrfritz.jolokiamunin.config.ConfigurationLoader} interface and
+ * register it through the {@code /META-INF/services/de.chrfritz.jolokiamunin.config.ConfigurationLoader} in your own jar
+ * within the classpath.
+ *
+ * @author christian.fritz
+ * @see java.util.ServiceLoader
+ */
+public class FileEndingConfigurationLoader implements ConfigurationLoader {
+
+    private Map<String, ConfigurationLoader> configurationLoaderMap = new HashMap<>();
+
+    /**
+     * Initialize the configuraton loader.
+     */
+    public FileEndingConfigurationLoader() {
+        ServiceLoader<ConfigurationLoader> serviceLoader = ServiceLoader.load(ConfigurationLoader.class);
+        for (ConfigurationLoader configurationLoader : serviceLoader) {
+            for (String fileEnding : configurationLoader.getAssignedFileExtensions()) {
+                if (!configurationLoaderMap.containsKey(fileEnding)) {
+                    configurationLoaderMap.put(fileEnding, configurationLoader);
+                }
+            }
+        }
+    }
+
+    /**
+     * Load a configuration from a specific file.
+     *
+     * @param configFile Load the configuration from this file.
+     * @return A configuration instance.
+     * @throws de.chrfritz.jolokiamunin.config.ConfigurationException In case of the configuration can not loaded.
+     */
+    @Override
+    public Configuration loadConfig(File configFile) throws ConfigurationException {
+        String fileName = configFile.getName();
+        String extension = fileName.substring(fileName.lastIndexOf('.') + 1);
+        if (!configurationLoaderMap.containsKey(extension)) {
+            throw new ConfigurationException(
+                    "Can not load configuration file '" + configFile +
+                            "'. There is no matching configuration loader available.\n" +
+                            "The file extensions can be loaded:\n" +
+                            StringUtils.join(getAssignedFileExtensions(), "\n  - ")
+            );
+        }
+        return configurationLoaderMap.get(extension).loadConfig(configFile);
+    }
+
+    /**
+     * Get a list of all file extensions which can be read by the implementing configuration loader.
+     *
+     * @return A list of all readable file extensions.
+     */
+    @Override
+    public List<String> getAssignedFileExtensions() {
+        return Lists.newArrayList(configurationLoaderMap.keySet());
+    }
+}
---
+++ src/test/java/de/chrfritz/jolokiamunin/config/FileEndingConfigurationLoaderTest.java
@@ -0,0 +1,56 @@
+// ______________________________________________________________________________
+//
+//           Project: jolokia-munin-plugin
+//            Module: jolokia-munin-plugin
+//             Class: FileEndingConfigurationLoaderTest
+//              File: FileEndingConfigurationLoaderTest.java
+//        changed by: christian.fritz
+//       change date: 29.11.14 17:25
+// ______________________________________________________________________________
+//
+//         Copyright: (c) Christian Fritz, all rights reserved
+// ______________________________________________________________________________
+
+package de.chrfritz.jolokiamunin.config;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+/**
+ * Unit test for {@link de.chrfritz.jolokiamunin.config.FileEndingConfigurationLoader}
+ *
+ * @author christian.fritz
+ */
+public class FileEndingConfigurationLoaderTest {
+    private ConfigurationLoader loader;
+
+    @Before
+    public void setUp() throws Exception {
+        loader = new FileEndingConfigurationLoader();
+    }
+
+    @Test(expected = ConfigurationException.class)
+    public void testLoadConfigInvalidExtension() throws Exception {
+        loader.loadConfig(new File("invalid.extension"));
+    }
+
+    @Test
+    public void testLoadConfig() throws Exception {
+        File configFile = new File(
+                getClass().getResource("/de/chrfritz/jolokiamunin/config/testConfig.groovy").toURI());
+        Configuration actual = loader.loadConfig(configFile);
+        assertThat(actual, is(notNullValue()));
+    }
+
+    @Test
+    public void testGetAssignedFileExtensions() throws Exception {
+        List<String> actual = loader.getAssignedFileExtensions();
+        assertThat(actual, contains("groovy", "xml"));
+    }
+}
