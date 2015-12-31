09532b802ad22347a333b35bc0e46a16013ec0c0
Christian Fritz <mail@chr-fritz.de>
2015-12-31 16:35:21 +0100

    Add missing Interface CliController

---
+++ jmp-api/src/main/java/de/chrfritz/jolokiamunin/api/CliController.java
@@ -0,0 +1,20 @@
+// ______________________________________________________________________________
+//
+//           Project: jolokia-munin-plugin
+//            Module: jmp-api
+//             Class: CliController
+//              File: CliController.java
+//        changed by: christian.fritz
+//       change date: 29.12.15 15:29
+// ______________________________________________________________________________
+//
+//         Copyright: (c) Christian Fritz, all rights reserved
+// ______________________________________________________________________________
+
+package de.chrfritz.jolokiamunin.api;
+
+/**
+ * @author christian.fritz
+ */
+public interface CliController extends Controller {
+}
